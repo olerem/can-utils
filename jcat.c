db4952891b1d7b07066b62f270f0941936833c2c
Oleksij Rempel <o.rempel@pengutronix.de>
2019-05-09 11:57:37 +0200

    m jcat.c
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>

--- jcat.c
+++ jcat.c
@@ -54,7 +54,6 @@ struct jcat_priv {
 	bool valid_peername;
 	bool todo_recv;
 	bool todo_filesize;
-	bool todo_connect;
 
 	unsigned long polltimeout;
 
@@ -64,6 +63,7 @@ struct jcat_priv {
 	struct sock_extended_err *serr;
 	struct scm_timestamping *tss;
 	struct jcat_stats stats;
+	uint8_t old_buf[8];
 };
 
 static const char help_msg[] =
@@ -71,16 +71,10 @@ static const char help_msg[] =
 	"Usage: jcat FROM TO\n"
 	" FROM / TO	- or [IFACE][:[SA][,[PGN][,NAME]]]\n"
 	"Options:\n"
-	" -i <infile>	(default stdin)\n"
-	" -s <size>	Set maximal transfer size. Default: 117440505 byte\n"
-	" -r		Receive data\n"
 	" -P <timeout>  poll timeout in milliseconds before sending data.\n"
 	"		With this option send() will be used with MSG_DONTWAIT flag.\n"
-	" -R <count>	Set send repeat count. Default: 1\n"
 	"\n"
 	"Example:\n"
-	"jcat -i some_file_to_send  can0:0x80 :0x90,0x12300\n"
-	"jcat can0:0x90 -r > /tmp/some_file_to_receive\n"
 	"\n"
 	;
 
@@ -95,215 +89,44 @@ static void jcat_init_sockaddr_can(struct sockaddr_can *sac)
 	sac->can_addr.j1939.pgn = J1939_NO_PGN;
 }
 
-static ssize_t jcat_send_one(struct jcat_priv *priv, int out_fd,
-			     const void *buf, size_t buf_size)
+static int jcat_recv_one(struct jcat_priv *priv)
 {
-	ssize_t num_sent;
-	int flags = 0;
+	int ret, i;
+	uint8_t buf[8];
+	bool new = false;
 
-	if (priv->polltimeout)
-		flags |= MSG_DONTWAIT;
-
-	if (priv->valid_peername && !priv->todo_connect)
-		num_sent = sendto(out_fd, buf, buf_size, flags,
-				  (struct sockaddr *)&priv->peername,
-				  sizeof(priv->peername));
-	else
-		num_sent = send(out_fd, buf, buf_size, flags);
-
-	if (num_sent == -1) {
-		warn("%s: transfer error: %i", __func__, -errno);
-		return -errno;
+	ret = recv(priv->sock, buf, sizeof(buf), 0);
+	if (ret < 0) {
+		warn("recvf()");
+		return EXIT_FAILURE;
 	}
 
-	if (num_sent == 0) /* Should never happen */ {
-		warn("%s: transferred 0 bytes", __func__);
-		return -EINVAL;
-	}
-
-	if (num_sent > buf_size) /* Should never happen */ {
-		warn("%s: send more then read", __func__);
-		return -EINVAL;
-	}
-
-	return num_sent;
-}
-
-static void jcat_print_timestamp(struct jcat_priv *priv, const char *name,
-			      struct timespec *cur)
-{
-	struct jcat_stats *stats = &priv->stats;
-
-	if (!(cur->tv_sec | cur->tv_nsec))
-		return;
-
-	fprintf(stderr, "  %s: %lu s %lu us (seq=%u, send=%u)",
-			name, cur->tv_sec, cur->tv_nsec / 1000,
-			stats->tskey, stats->send);
-
-	fprintf(stderr, "\n");
-}
-
-static const char *jcat_tstype_to_str(int tstype)
-{
-	switch (tstype) {
-	case SCM_TSTAMP_SCHED:
-		return "  ENQ";
-	case SCM_TSTAMP_SND:
-		return "  SND";
-	case SCM_TSTAMP_ACK:
-		return "  ACK";
-	default:
-		return "  unk";
-	}
-}
-
-/* Check the stats of SCM_TIMESTAMPING_OPT_STATS */
-static void jcat_scm_opt_stats(struct jcat_priv *priv, void *buf, int len)
-{
-	struct jcat_stats *stats = &priv->stats;
-	int offset = 0;
-
-	while (offset < len) {
-		struct nlattr *nla = (struct nlattr *) (buf + offset);
-
-		switch (nla->nla_type) {
-		case J1939_NLA_BYTES_ACKED:
-			stats->send = *(uint32_t *)((void *)nla + NLA_HDRLEN);
+	for (i = 0; i < sizeof(buf); i++) {
+		if (priv->old_buf[i] != buf[i]) {
+			new = true;
 			break;
-		default:
-			warnx("not supported J1939_NLA field\n");
+		}
+	}
+
+	if (new) {
+		for (i = 0; i < sizeof(buf); i++) {
+			fprintf(stdout, "0x%02x ", buf[i]);
+			priv->old_buf[i] = buf[i];
 		}
 
-		offset += NLA_ALIGN(nla->nla_len);
-	}
-}
-
-static int jcat_extract_serr(struct jcat_priv *priv)
-{
-	struct jcat_stats *stats = &priv->stats;
-	struct sock_extended_err *serr = priv->serr;
-	struct scm_timestamping *tss = priv->tss;
-
-	switch (serr->ee_origin) {
-	case SO_EE_ORIGIN_TIMESTAMPING:
-		/*
-		 * We expect here following patterns:
-		 *   serr->ee_info == SCM_TSTAMP_ACK
-		 *     Activated with SOF_TIMESTAMPING_TX_ACK
-		 * or
-		 *   serr->ee_info == SCM_TSTAMP_SCHED
-		 *     Activated with SOF_TIMESTAMPING_SCHED
-		 * and
-		 *   serr->ee_data == tskey
-		 *     session message counter which is activate
-		 *     with SOF_TIMESTAMPING_OPT_ID
-		 * the serr->ee_errno should be ENOMSG
-		 */
-		if (serr->ee_errno != ENOMSG)
-			warnx("serr: expected ENOMSG, got: %i",
-			      serr->ee_errno);
-		stats->tskey = serr->ee_data;
-
-		jcat_print_timestamp(priv, jcat_tstype_to_str(serr->ee_info),
-				     &tss->ts[0]);
-
-		if (serr->ee_info == SCM_TSTAMP_SCHED)
-			return -EINTR;
-		else
-			return 0;
-	case SO_EE_ORIGIN_LOCAL:
-		/*
-		 * The serr->ee_origin == SO_EE_ORIGIN_LOCAL is
-		 * currently used to notify about locally
-		 * detected protocol/stack errors.
-		 * Following patterns are expected:
-		 *   serr->ee_info == J1939_EE_INFO_TX_ABORT
-		 *     is used to notify about session TX
-		 *     abort.
-		 *   serr->ee_data == tskey
-		 *     session message counter which is activate
-		 *     with SOF_TIMESTAMPING_OPT_ID
-		 *   serr->ee_errno == actual error reason
-		 *     error reason is converted from J1939
-		 *     abort to linux error name space.
-		 */
-		if (serr->ee_info != J1939_EE_INFO_TX_ABORT)
-			warnx("serr: unknown ee_info: %i",
-			      serr->ee_info);
-
-		jcat_print_timestamp(priv, "  ABT", &tss->ts[0]);
-		warnx("serr: tx error: %i, %s", serr->ee_errno, strerror(serr->ee_errno));
-
-		return serr->ee_errno;
-	default:
-		warnx("serr: wrong origin: %u", serr->ee_origin);
+		fprintf(stdout, "\n");
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
 
-static int jcat_parse_cm(struct jcat_priv *priv, struct cmsghdr *cm)
+static int jcat_recv_loop(struct jcat_priv *priv)
 {
-	const size_t hdr_len = CMSG_ALIGN(sizeof(struct cmsghdr));
+	unsigned int events = POLLIN;
+	bool abort = false;
+	int ret = 0;
 
-	if (cm->cmsg_level == SOL_SOCKET && cm->cmsg_type == SCM_TIMESTAMPING) {
-		priv->tss = (void *)CMSG_DATA(cm);
-	} else if (cm->cmsg_level == SOL_SOCKET && cm->cmsg_type == SCM_TIMESTAMPING_OPT_STATS) {
-		void *jstats = (void *)CMSG_DATA(cm);
-
-		/* Activated with SOF_TIMESTAMPING_OPT_STATS */
-		jcat_scm_opt_stats(priv, jstats, cm->cmsg_len - hdr_len);
-	} else if (cm->cmsg_level == SOL_CAN_J1939 &&
-		   cm->cmsg_type == SCM_J1939_ERRQUEUE) {
-		priv->serr = (void *)CMSG_DATA(cm);
-	} else
-		warnx("serr: not supported type: %d.%d",
-		      cm->cmsg_level, cm->cmsg_type);
-
-	return 0;
-}
-
-static int jcat_recv_err(struct jcat_priv *priv)
-{
-	char control[100];
-	struct cmsghdr *cm;
-	int ret;
-	struct msghdr msg = {
-		.msg_control = control,
-		.msg_controllen = sizeof(control),
-	};
-
-	ret = recvmsg(priv->sock, &msg, MSG_ERRQUEUE);
-	if (ret == -1)
-		err(EXIT_FAILURE, "recvmsg error notification: %i", errno);
-	if (msg.msg_flags & MSG_CTRUNC)
-		err(EXIT_FAILURE, "recvmsg error notification: truncated");
-
-	priv->serr = NULL;
-	priv->tss = NULL;
-
-	for (cm = CMSG_FIRSTHDR(&msg); cm && cm->cmsg_len;
-	     cm = CMSG_NXTHDR(&msg, cm)) {
-		jcat_parse_cm(priv, cm);
-		if (priv->serr && priv->tss)
-			return jcat_extract_serr(priv);
-	}
-
-	return 0;
-}
-
-static int jcat_send_loop(struct jcat_priv *priv, int out_fd, char *buf,
-			  size_t buf_size)
-{
-	ssize_t count, num_sent = 0;
-	char *tmp_buf = buf;
-	unsigned int events = POLLOUT | POLLERR;
-	bool tx_done = false;
-
-	count = buf_size;
-
-	while (!tx_done) {
+	while (!abort) {
 		if (priv->polltimeout) {
 			struct pollfd fds = {
 				.fd = priv->sock,
@@ -324,194 +147,22 @@ static int jcat_send_loop(struct jcat_priv *priv, int out_fd, char *buf,
 				return -EIO;
 			}
 
-			if (fds.revents & POLLERR) {
-				ret = jcat_recv_err(priv);
-				if (ret == -EINTR)
-					continue;
-				else if (ret)
+			if (fds.revents & POLLIN) {
+				ret = jcat_recv_one(priv);
+				if (ret < 0)
 					return ret;
-				else
-					tx_done = true;
-
-			}
-
-			if (fds.revents & POLLOUT) {
-				num_sent = jcat_send_one(priv, out_fd, tmp_buf, count);
-				if (num_sent < 0)
-					return num_sent;
 			}
 		} else {
-			num_sent = jcat_send_one(priv, out_fd, tmp_buf, count);
-			if (num_sent < 0)
-				return num_sent;
+			ret = jcat_recv_one(priv);
+			if (ret < 0)
+				return ret;
 		}
-
-		count -= num_sent;
-		tmp_buf += num_sent;
-		if (buf + buf_size < tmp_buf + count) {
-			warn("%s: send buffer is bigger than the read buffer",
-			     __func__);
-			return -EINVAL;
-		}
-		if (!count)
-			events = POLLERR;
 	}
 	return 0;
 }
 
-static int jcat_sendfile(struct jcat_priv *priv, int out_fd, int in_fd,
-			 off_t *offset, size_t count)
-{
-	int ret = EXIT_SUCCESS;
-	off_t orig = 0;
-	char *buf;
-	size_t to_read, num_read, buf_size;
-
-	buf_size = min(priv->max_transfer, count);
-	buf = malloc(buf_size);
-	if (!buf) {
-		warn("can't allocate buf");
-		ret = EXIT_FAILURE;
-		goto do_nofree;
-	}
-
-	if (offset) {
-
-		/* Save current file offset and set offset to value in '*offset' */
-
-		orig = lseek(in_fd, 0, SEEK_CUR);
-		if (orig == -1) {
-			ret = EXIT_FAILURE;
-			goto do_free;
-		}
-		if (lseek(in_fd, *offset, SEEK_SET) == -1) {
-			ret = EXIT_FAILURE;
-			goto do_free;
-		}
-	}
-
-	while (count > 0) {
-		to_read = min(buf_size, count);
-
-		num_read = read(in_fd, buf, to_read);
-		if (num_read == -1) {
-			ret = EXIT_FAILURE;
-			goto do_free;
-		}
-		if (num_read == 0)
-			break; /* EOF */
-
-		ret = jcat_send_loop(priv, out_fd, buf, num_read);
-		if (ret)
-			goto do_free;
-
-		count -= num_read;
-	}
-
-	if (offset) {
-		/* Return updated file offset in '*offset', and reset the file offset
-		   to the value it had when we were called. */
-
-		*offset = lseek(in_fd, 0, SEEK_CUR);
-		if (*offset == -1) {
-			ret = EXIT_FAILURE;
-			goto do_free;
-		}
-
-		if (lseek(in_fd, orig, SEEK_SET) == -1) {
-			ret = EXIT_FAILURE;
-			goto do_free;
-		}
-	}
-
-do_free:
-	free(buf);
-do_nofree:
-	return ret;
-}
-
-static size_t jcat_get_file_size(int fd)
-{
-	off_t offset;
-
-	offset = lseek(fd, 0, SEEK_END);
-	if (offset == -1)
-		error(1, errno, "%s lseek()\n", __func__);
-
-	if (lseek(fd, 0, SEEK_SET) == -1)
-		error(1, errno, "%s lseek() start\n", __func__);
-
-	return offset;
-}
-
-static int jcat_send(struct jcat_priv *priv)
-{
-	unsigned int size = 0;
-	int ret, i;
-
-	if (priv->todo_filesize)
-		size = jcat_get_file_size(priv->infile);
-
-	if (!size)
-		return EXIT_FAILURE;
-
-	for (i = 0; i < priv->repeat; i++) {
-		ret = jcat_sendfile(priv, priv->sock, priv->infile, NULL, size);
-		if (ret)
-			break;
-
-		if (lseek(priv->infile, 0, SEEK_SET) == -1)
-			error(1, errno, "%s lseek() start\n", __func__);
-	}
-
-	return ret;
-}
-
-static int jcat_recv_one(struct jcat_priv *priv, uint8_t *buf, size_t buf_size)
-{
-	int ret;
-
-	ret = recv(priv->sock, buf, buf_size, 0);
-	if (ret < 0) {
-		warn("recvf()");
-		return EXIT_FAILURE;
-	}
-
-	ret = write(priv->outfile, buf, ret);
-	if (ret < 0) {
-		warn("write stdout()");
-		return EXIT_FAILURE;
-	}
-
-	return EXIT_SUCCESS;
-}
-
-static int jcat_recv(struct jcat_priv *priv)
-{
-	int ret = EXIT_SUCCESS;
-	size_t buf_size;
-	uint8_t *buf;
-
-	buf_size = priv->max_transfer;
-	buf = malloc(buf_size);
-	if (!buf) {
-		warn("can't allocate rx buf");
-		return EXIT_FAILURE;;
-	}
-
-	while (priv->todo_recv) {
-		ret = jcat_recv_one(priv, buf, buf_size);
-		if (ret)
-			break;
-	}
-
-	free(buf);
-	return ret;
-}
-
 static int jcat_sock_prepare(struct jcat_priv *priv)
 {
-	unsigned int sock_opt;
 	int value;
 	int ret;
 
@@ -532,32 +183,17 @@ static int jcat_sock_prepare(struct jcat_priv *priv)
 	}
 
 	value = 1;
-	ret = setsockopt(priv->sock, SOL_CAN_J1939, SO_J1939_ERRQUEUE, &value,
-			 sizeof(value));
-	if (ret < 0) {
-		warn("set recverr");
-		return EXIT_FAILURE;
-	}
+	ret = setsockopt(priv->sock, SOL_SOCKET, SO_BROADCAST,
+			&value, sizeof(value));
+	if (ret < 0)
+		error(1, errno, "setsockopt set broadcast");
 
-	sock_opt = SOF_TIMESTAMPING_SOFTWARE |
-		   SOF_TIMESTAMPING_OPT_CMSG |
-		   SOF_TIMESTAMPING_TX_ACK |
-		   SOF_TIMESTAMPING_TX_SCHED |
-		   SOF_TIMESTAMPING_OPT_STATS | SOF_TIMESTAMPING_OPT_TSONLY |
-		   SOF_TIMESTAMPING_OPT_ID;
-
-	if (setsockopt(priv->sock, SOL_SOCKET, SO_TIMESTAMPING,
-		       (char *) &sock_opt, sizeof(sock_opt)))
-		error(1, 0, "setsockopt timestamping");
 
 	ret = bind(priv->sock, (void *)&priv->sockname, sizeof(priv->sockname));
 	if (ret < 0) {
 		warn("bind()");
 		return EXIT_FAILURE;
 	}
-
-	if (!priv->todo_connect)
-		return EXIT_SUCCESS;
 
 	if (!priv->valid_peername) {
 		warn("no peername supplied");
@@ -580,29 +216,8 @@ static int jcat_parse_args(struct jcat_priv *priv, int argc, char *argv[])
 	/* argument parsing */
 	while ((opt = getopt(argc, argv, optstring)) != -1)
 	switch (opt) {
-	case 'i':
-		priv->infile = open(optarg, O_RDONLY);
-		if (priv->infile == -1)
-			error(EXIT_FAILURE, errno, "can't open input file");
-		priv->todo_filesize = 1;
-		break;
-	case 's':
-		priv->max_transfer = strtoul(optarg, NULL, 0);
-		if (priv->max_transfer > J1939_MAX_ETP_PACKET_SIZE)
-			err(EXIT_FAILURE, "used value (%zu) is bigger then allowed maximal size: %u.\n",
-			    priv->max_transfer, J1939_MAX_ETP_PACKET_SIZE);
-		break;
-	case 'r':
-		priv->todo_recv = 1;
-		break;
-	case 'p':
-		priv->todo_prio = strtoul(optarg, NULL, 0);
-		break;
 	case 'P':
 		priv->polltimeout = strtoul(optarg, NULL, 0);
-		break;
-	case 'c':
-		priv->todo_connect = 1;
 		break;
 	case 'R':
 		priv->repeat = atoi(optarg);
@@ -660,10 +275,7 @@ int main(int argc, char *argv[])
 	if (ret)
 		return ret;
 
-	if (priv->todo_recv)
-		ret = jcat_recv(priv);
-	else
-		ret = jcat_send(priv);
+	ret = jcat_recv_loop(priv);
 
 	close(priv->infile);
 	close(priv->outfile);
